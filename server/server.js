081a99caea6636a7cc6525b8b512375c2734ddff
Vitaliy <puga.vitaliy@gmail.com>
2018-12-31 10:17:42 +0200

    апи для logout

--- server/server.js
+++ server/server.js
@@ -62,6 +62,20 @@ app.post('/api/users/login', (req, res) => {
     });
 });
 
+app.get('/api/user/logout', auth, (req, res) => {
+    User.findOneAndUpdate(
+        {_id: req.user._id},
+        {token: ''},
+        (err, doc) => {
+            if (err) return res.json({success: false, err});
+            return res.status(200).send({
+                success: true
+            });
+        }
+    );
+    console.log(req.body)
+});
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
